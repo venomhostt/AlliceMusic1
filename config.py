d2d4823d0db7fbffdfa19ea8c3d42f1c4779f6be
Venom <venomhost.in@gmail.com>
2026-03-19 18:46:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Venomvipbots")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Venompratapchat")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+yFY3ysqq-5NjMDhl")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/stylishdpforishu")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
